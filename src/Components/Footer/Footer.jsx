279099b8db689013c69ff3f17319ac3d2dab33e0
Prithibee13 <azizprithibee1213@gmail.com>
2023-11-28 23:12:40 +0600

    Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,27 @@
 const Footer = () =>
 {
-
+   return( 
+   <footer className="container">
+        <div className="row gap-5">
+            <div className="col">
+                <h4><span>Food</span><span>man</span></h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, veritatis saepe facere repellendus totam consectetur?</p>
+                <i class="fa-brands fa-facebook-f fa-2x"></i>
+                <i class="fa-brands fa-twitter fa-2x"></i>
+                <i class="fa-brands fa-instagram fa-2x"></i>
+            </div>
+            <div className="col">
+                <h5>Products</h5>
+            </div>
+            <div className="col">
+                <h5>Information</h5>
+            </div>
+            <div className="col">
+                <h5>Company</h5>
+            </div>
+        </div>
+    </footer>
+    );
 }
 
 export default Footer
